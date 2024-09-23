970d886cf2dd2a17e62f848a136bd8edd60f3e2f
Artem Smushkov <artyom.smushkov@gmail.com>
2024-09-23 15:39:53 +0300

    Disable zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,3 @@ end
 #oh-my-posh init fish --config ~/.local/share/oh-my-posh-themes/powerlevel10k_rainbow.omp.json | source
 starship init fish | source
 set -g fish_greeting
-
-zoxide init fish --cmd cd | source
