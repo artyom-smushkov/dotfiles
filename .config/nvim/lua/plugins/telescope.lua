a09b70ce70554ac7d2486e92aa02e0b65cc6835a
Artem Smushkov <artyom.smushkov@gmail.com>
2024-12-25 18:52:02 +0200

    Update neovim config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -25,6 +25,7 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
             vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>s", builtin.current_buffer_fuzzy_find, {})
             vim.keymap.set("n", "<leader>b", builtin.buffers, {})
             require("telescope").load_extension("ui-select")
         end,
