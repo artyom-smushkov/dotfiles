00f7fd0af337fb411a0cfef1c77da679f333fbb1
Artem Smushkov <artyom.smushkov@gmail.com>
2024-12-27 13:50:03 +0200

    Update neovim config

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -10,6 +10,18 @@ return {
         neogit.setup({
             integrations = {
                 telescope = true
+            },
+            graph_style = "unicode",
+            commit_editor = {
+                kind = "tab",
+                show_staged_diff = true,
+                -- Accepted values:
+                -- "split" to show the staged diff below the commit editor
+                -- "vsplit" to show it to the right
+                -- "split_above" Like :top split
+                -- "vsplit_left" like :vsplit, but open to the left
+                -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
+                staged_diff_split_kind = "vsplit_left",
             }
         })
         vim.keymap.set("n", "<leader>g", function() neogit.open({ kind = 'vsplit' }) end, {})
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,15 +3,19 @@ return {
         "nvim-telescope/telescope-ui-select.nvim",
     },
     {
+        "nvim-telescope/telescope-fzf-native.nvim", build="make"
+    },
+    {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = { "nvim-lua/plenary.nvim" , "nvim-telescope/telescope-fzf-native.nvim"},
         config = function()
             require("telescope").setup({
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown({}),
                     },
+                    fzf  = {},
                 },
                 pickers = {
                     buffers = {
@@ -22,6 +26,7 @@ return {
                     }
                 },
             })
+            require("telescope").load_extension("fzf")
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
             vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
